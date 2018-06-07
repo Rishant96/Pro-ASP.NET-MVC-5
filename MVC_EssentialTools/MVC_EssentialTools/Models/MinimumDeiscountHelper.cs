7e8f559fc18aff704dfce5700b64d11c5da7d774
Rishant Dutt <kirarishant@outlook.com>
2018-06-07 13:07:26 +0530

    #6 Unit Testing: Tests Init

---
+++ MVC_EssentialTools/MVC_EssentialTools/Models/MinimumDeiscountHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_EssentialTools.Models
+{
+    public class MinimumDeiscountHelper : IDiscountHelper 
+    {
+        public decimal ApplyDiscount(decimal totalparam)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
